f932179054969fbf5a95aa8516d56774b090d456
denilson144 <denilson144@gmail.com>
2025-08-18 21:37:37 -0300

    Create aula_03 .c

---
+++ aula_03 .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int valor_a;
+int valor_b; 
+int valor_c;
+
+prinft("digite e valor a");
+scanf("%d,%valor_b ")
+   
+   prinft("digite e valor b");
+scanf("%d,%valor_c ")
+
+
+  prinft("digite e valor a");
+scanf("%d,%valor_c ")
+
+  prinft("digite e valor c");
+scanf("%d,%valor_b ")
