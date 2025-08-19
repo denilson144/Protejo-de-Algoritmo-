abbf901a2ed8b1f4045e3c64a63853d21ec2a693
denilson144 <denilson144@gmail.com>
2025-08-18 21:37:48 -0300

    Create aula_03_01.c

---
+++ aula_03_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+   //Definição de variavel
+   int idade = 27;
+    printf("digite a sua idade:");
+    scanf("%d",%idade);  
+    //saida de valor 
+    printf("Sua idade e %d\n", idade);
+    printf("minha politica de memoria e %d\n", idade);
+    return 0;
+} 
